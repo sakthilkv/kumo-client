680a8d8484765bf378328a3065e852728b9a76e3
sakthilkv <sakthi13072005@gmail.com>
2024-12-07 01:29:17 +0530

    feat: added api for dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,175 +1,93 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ControlBar from '@/components/ControlBar'
 import CardGrid from '@/components/CardGrid'
 import { Link } from 'react-router-dom'
+interface MediaContent {
+	id: string
+	media_type: string
+	plot: string
+	poster_url: string
+	title: string
+	year: string
+}
 
-export default function Dashboard() {
-	const [selectedCategory, setSelectedCategory] = useState<string>('movie')
-	const [searchQuery, setSearchQuery] = useState<string>('')
-	const [selectedStatus, setSelectedStatus] = useState<string>('all')
-	const mediaContent = [
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@.jpg',
-			name: 'The Grand Budapest Hotel',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BM2ZmNjQ2MzAtNDlhNi00MmQyLWJhZDMtNmJiMjFlOWY4MzcxXkEyXkFqcGc@.jpg',
-			name: 'Guardians of the Galaxy',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@.jpg',
-			name: 'Into the Spider-Verse',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjIzMzA1OTkzNV5BMl5BanBnXkFtZTgwODE3MjM4NzE@.jpg',
-			name: 'The Secret Life of Pets',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BNTg2OTY2ODg5OF5BMl5BanBnXkFtZTcwODM5MTYxOA@@.jpg',
-			name: 'Life of Pi',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMDIyM2E2NTAtMzlhNy00ZGUxLWI1NjgtZDY5MzhiMDc5NGU3XkEyXkFqcGc@.jpg',
-			name: 'Coco',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BZDRkODJhOTgtOTc1OC00NTgzLTk4NjItNDgxZDY4YjlmNDY2XkEyXkFqcGc@.jpg',
-			name: 'Mad Max: Fury Road',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BYjBkOWUwODYtYWI3YS00N2I0LWEyYTktOTJjM2YzOTc3ZDNlXkEyXkFqcGc@.jpg',
-			name: 'The Lion King',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjQ2ODIyMjY4MF5BMl5BanBnXkFtZTgwNzY4ODI2NzM@.jpg',
-			name: 'Aladdin',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@.jpg',
-			name: 'Doctor Strange',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMTg4MDk1ODExN15BMl5BanBnXkFtZTgwNzIyNjg3MDE@.jpg',
-			name: 'The Lego Movie',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@.jpg',
-			name: 'Thor: Ragnarok',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@.jpg',
-			name: 'The Grand Budapest Hotel',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BM2ZmNjQ2MzAtNDlhNi00MmQyLWJhZDMtNmJiMjFlOWY4MzcxXkEyXkFqcGc@.jpg',
-			name: 'Guardians of the Galaxy',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@.jpg',
-			name: 'Into the Spider-Verse',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjIzMzA1OTkzNV5BMl5BanBnXkFtZTgwODE3MjM4NzE@.jpg',
-			name: 'The Secret Life of Pets',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BNTg2OTY2ODg5OF5BMl5BanBnXkFtZTcwODM5MTYxOA@@.jpg',
-			name: 'Life of Pi',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMDIyM2E2NTAtMzlhNy00ZGUxLWI1NjgtZDY5MzhiMDc5NGU3XkEyXkFqcGc@.jpg',
-			name: 'Coco',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BZDRkODJhOTgtOTc1OC00NTgzLTk4NjItNDgxZDY4YjlmNDY2XkEyXkFqcGc@.jpg',
-			name: 'Mad Max: Fury Road',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BYjBkOWUwODYtYWI3YS00N2I0LWEyYTktOTJjM2YzOTc3ZDNlXkEyXkFqcGc@.jpg',
-			name: 'The Lion King',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjQ2ODIyMjY4MF5BMl5BanBnXkFtZTgwNzY4ODI2NzM@.jpg',
-			name: 'Aladdin',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@.jpg',
-			name: 'Doctor Strange',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMTg4MDk1ODExN15BMl5BanBnXkFtZTgwNzIyNjg3MDE@.jpg',
-			name: 'The Lego Movie',
-			type: 'movie',
-		},
-		{
-			id: '1',
-			url: 'https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@.jpg',
-			name: 'Thor: Ragnarok',
-			type: 'movie',
-		},
-	]
-	const cards = mediaContent.map((item, i) => (
-		<Link to={`/${item.type}/${item.id}`} className="flex items-center">
-			<div key={i} className="flex flex-col items-left">
+const Card: React.FC<{ item: MediaContent }> = ({ item }) => {
+	return (
+		<Link to={`/${item.media_type}/${item.id}`} className="flex items-center" key={item.id}>
+			<div className="flex flex-col items-left">
 				<div
 					className="p-4 h-80 w-44 sm:w-1/2 md:w-48 border rounded-lg shadow-md flex items-center justify-center"
 					style={{
-						backgroundImage: `url('${item.url}')`,
+						backgroundImage: `url('https://image.tmdb.org/t/p/w600_and_h900_bestv2${item.poster_url}')`,
 						backgroundSize: 'cover',
 						backgroundPosition: 'center',
 						backgroundRepeat: 'no-repeat',
 					}}
 				></div>
-				<p className="mt-3 font-semibold text-left  line-clamp-2 ml-1">{item.name}</p>
-				<p className="font-semibold text-left text-sm  text-gray-500 line-clamp-2 ml-1">2019</p>
+				<p className="mt-3 font-semibold text-left line-clamp-2 ml-1">{item.title}</p>
+				<p className="font-semibold text-left text-sm text-gray-500 line-clamp-2 ml-1">
+					{item.year}
+				</p>
 			</div>
 		</Link>
-	))
+	)
+}
+
+export default function Dashboard() {
+	const [selectedCategory, setSelectedCategory] = useState<string>('movie')
+	const [searchQuery, setSearchQuery] = useState<string>('')
+	const [selectedStatus, setSelectedStatus] = useState<string>('all')
+	const [mediaContent, setMediaContent] = useState<MediaContent[]>([])
+
+	useEffect(() => {
+		const movieIds = [
+			'tt0111161',
+			'tt0068646',
+			'tt0468569',
+			'tt0071562',
+			'tt0050083',
+			'tt0108052',
+			'tt0816692',
+			'tt5311514',
+			'tt0167260',
+			'tt0137523',
+			'tt0133093',
+			'tt0169547',
+			'tt0080684',
+			'tt0099685',
+			'tt0076759',
+			'tt9426210',
+			'tt0086190',
+			'tt0109830',
+			'tt0134074',
+			'tt0253474',
+			'tt0211915',
+			'tt0102926',
+			'tt0110912',
+			'tt0266543',
+			'tt0372784',
+			'tt0038650',
+			'tt0114369',
+			'tt0325980',
+			'tt0482571',
+			'tt0317248',
+			'tt0120737',
+			'tt0816692',
+		]
+
+		const fetchData = async () => {
+			const data = await Promise.all(
+				movieIds.map(async (id) => {
+					const response = await fetch(`http://192.168.0.104:5000/media/info/movie/${id}`)
+					const json = await response.json()
+					return json
+				}),
+			)
+			setMediaContent(data)
+		}
+
+		fetchData()
+	}, [])
 
 	return (
 		<div className="flex flex-col justify-center w-full pl-2 pr-2 m-0 p-0 mb-5">
@@ -187,7 +105,11 @@ export default function Dashboard() {
 					<h1 className="text-xl font-bold">Status: {selectedStatus}</h1>
 				</div> */}
 			<div className="flex justify-center mt-10 ml-2 md:ml-12">
-				<CardGrid>{cards}</CardGrid>
+				<CardGrid>
+					{mediaContent.map((item) => (
+						<Card key={item.id} item={item} />
+					))}
+				</CardGrid>
 			</div>
 		</div>
 	)
